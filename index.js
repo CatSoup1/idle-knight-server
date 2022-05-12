5fc462332f27e50d14e5d8235a59356a207360c3
Definitely not xl_83 <98920372+LX8369@users.noreply.github.com>
2022-05-12 13:41:30 +1000

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+var CryptoJS = require("crypto-js");
 
-    let { DB } = require("mongquick");
+let { DB } = require("mongquick");
 const mongoSec = process.env.mongoSec
-
- const mdb = new DB(`mongodb+srv://xl83:${mongoSec}@cluster0.c2sln.mongodb.net/Cluster0?retryWrites=true&w=majority`);
+const mdb = new DB(`mongodb+srv://xl83:${mongoSec}@cluster0.c2sln.mongodb.net/Cluster0?retryWrites=true&w=majority`);
 
 
 const io = require("socket.io")(server, {
@@ -23,12 +23,15 @@ io.on('connection', async(socket) => {
   
   socket.on('sendData', async(data) => {
   if data.type == "signup" {
-     var check =  await mdb.get(data.username)
+     var check =  await mdb.get(`data-${data.username}`)
      if (check == null) {
-
+     var hash = CryptoJS.SHA256(data.key).toString();
+     mdb.set(`data-${data.username}`, hash);
     } else {
     socket.emit("usernameTaken")
     }
+  } else {
+  
   }
   
 })
